fafb7b26b4c9ea91ddbdaa7cd48ae70a4fd61cf0
dwids <sidwell.david@gmail.com>
2018-10-19 16:54:37 +1100

    Update hello.py
    Changed first print line

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-print("Hello, World!")
+print("Hello, World. I was changed on the Web at Github!")
 # first 2 lines added on Github via web editor. Initially in dev branch of my vsc repo
 print("hello from the PC - Visual Studio Code")
 # above line added on PC as you can guess
