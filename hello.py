ba4fdbbec372251605cddcc2e0201381e6ac49d2
David Sidwell <sidwell.david@gmail.com>
2018-10-19 16:52:12 +1100

    Added Hello from the PC line

--- hello.py
+++ hello.py
@@ -1,2 +1,4 @@
 print("Hello, World!")
 # first 2 lines added on Github via web editor. Initially in dev branch of my vsc repo
+print("hello from the PC - Visual Studio Code")
+# above line added on PC as you can guess
